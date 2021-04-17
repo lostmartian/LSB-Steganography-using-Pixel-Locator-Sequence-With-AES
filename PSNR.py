8a7806ebdbc36d82f5d37b1f44940e4c3fb97f88
lostmartian <sahilgangurde08@gmail.com>
2021-04-17 19:39:32 +0530

    Added steganalysis results

--- PSNR.py
+++ PSNR.py
@@ -2,9 +2,10 @@ from math import log10, sqrt
 import cv2
 import numpy as np
 
-
 def PSNR(original, compressed):
     mse = np.mean((original - compressed) ** 2)
+    err = np.sum((original.astype("float") - compressed.astype("float")) ** 2)
+    err /= float(original.shape[0] * original.shape[1])
     if (mse == 0):
         return 100
     max_pixel = 255.0
@@ -13,24 +14,24 @@ def PSNR(original, compressed):
     return psnr
 
 def main():
-    original = cv2.imread("images/out1.png")
-    compressed = cv2.imread("images/in1.png", 1)
+    original = cv2.imread("images/in1.png")
+    compressed = cv2.imread("images/out1.png")
     value = PSNR(original, compressed)
     print(f"1st PSNR value is {value} dB")
-    original = cv2.imread("images/in2.png")
-    compressed = cv2.imread("images/out2.png", 1)
-    value = PSNR(original, compressed)
-    print(f"2nd PSNR value is {value} dB")
+    # original = cv2.imread("images/in2.png")
+    # compressed = cv2.imread("images/out2.png")
+    # value = PSNR(original, compressed)
+    # print(f"2nd PSNR value is {value} dB")
     original = cv2.imread("images/in3.png")
-    compressed = cv2.imread("images/out3.png", 1)
+    compressed = cv2.imread("images/out3.png")
     value = PSNR(original, compressed)
     print(f"3rd PSNR value is {value} dB")
     original = cv2.imread("images/in4.png")
-    compressed = cv2.imread("images/out4.png", 1)
+    compressed = cv2.imread("images/out4.png")
     value = PSNR(original, compressed)
     print(f"4th PSNR value is {value} dB")
     original = cv2.imread("images/in5.png")
-    compressed = cv2.imread("images/out5.png", 1)
+    compressed = cv2.imread("images/out5.png")
     value = PSNR(original, compressed)
     print(f"5th PSNR value is {value} dB")
 
